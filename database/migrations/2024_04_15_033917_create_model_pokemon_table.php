66103b365e8dd6654e631d1c00c63ba4ff223c3c
Denis Fournier de Carvalho <denisfournier1994@gmail.com>
2024-04-15 02:45:11 -0300

    chore: Initializing ptoject and injecting dependencies. Including models.

---
+++ database/migrations/2024_04_15_033917_create_model_pokemon_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pokemon', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type1');
+            $table->string('typ2');
+            $table->string('height');
+            $table->string('weight');
+            $table->string('sprite');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pokemon');
+    }
+};
